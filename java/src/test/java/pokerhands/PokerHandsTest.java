95c9a34b932dcb56f12c76fe970c760179aa5896
Tony Vo <ttrung.vo@gmail.com>
2022-02-26 08:00:16 -0700

    poker hand initial state

---
+++ java/src/test/java/pokerhands/PokerHandsTest.java
@@ -0,0 +1,16 @@
+package pokerhands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Tony on 1/15/2016.
+ */
+public class PokerHandsTest {
+
+    @Test
+    public void sampleTest() throws Exception {
+        assertEquals( true, false);
+    }
+}
